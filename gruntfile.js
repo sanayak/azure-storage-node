9f128b2ed81ebe86b6cbdf6e6c34f149f60ab4d3
sanayak@microsoft.com <sanayak@microsoft.com>
2017-11-29 23:59:50 -0800

    Updating the grunt file to run only table tests

--- gruntfile.js
+++ gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
           clearRequireCache: false,
           timeout: 100000
         },
-        src: ['test/**/*.js']
+        src: ['test/services/table/*.js']
       }
     },
 
